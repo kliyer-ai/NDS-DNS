8395d659c52c7b7710ee3ca5cda75786d3c25764
Jo <J.Lowe@student.ru.nl>
2018-04-22 19:32:26 +0200

    redo sw

--- dns/SocketWrapper.py
+++ dns/SocketWrapper.py
@@ -3,8 +3,10 @@ import socket
 from dns import message
 import queue
 import select
+import threading
 
-class SocketWrapper:
+
+class SocketWrapper(threading.Thread):
     readlock = Lock()
     msgs = {}
     q = queue.Queue(42)
@@ -12,16 +14,22 @@ class SocketWrapper:
     def __init__(self, port):
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.bind(('localhost',self.port))
+        self.sock.bind(('localhost', self.port))
         self.sock.settimeout(0.1)
         self.close = False
+
+    def run(self):
+        while not self.close:
+            self.listen()
+            self.flush_send()
+
     def listen(self):
         if self.close:
             self.sock.close()
             return
         r, _, _ = select.select([self.sock], [], [], 0.1)
         if r:
-            data, addr = self.sock.recvfrom(1024,)
+            data, addr = self.sock.recvfrom(1024, )
             msg = message.Message.from_bytes(data)
             id = msg.header.ident
             if id in self.msgs:
@@ -29,26 +37,25 @@ class SocketWrapper:
             else:
                 self.msgs[id] = [msg]
 
+    def flush_send(self):
+        while not self.q.empty():
+            i = self.q.get()
+            self.sock.sendto(i[0].to_bytes(), (i[1], i[2]))
+
     def msgThere(self, id):
+        idMsgs = []
         with self.readlock:
             if id in self.msgs:
                 idMsgs = self.msgs[id]
                 self.msgs[id] = []
-                return idMsgs
-            return []
+        return idMsgs
 
-    def flush_send(self):
-        while not self.q.empty():
-            i = self.q.get()
-            self.sock.sendto(i[0].to_bytes(), (i[1],i[2]))
-        self.listen()
     def send(self, msg):
         """
-
         :param msg: tuple of shape (msg, ip, port)
         :return:
         """
         self.q.add(msg)
+
     def shotdown(self):
         self.close = True
-
